e89030806c4e345b9f3c1dbd7096a605c3c0d5ff
Victor R. Silva <victorr.silva005@gmail.com>
2025-09-11 16:26:54 -0300

    Criado projeto base

---
+++ server/core/dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,9 @@
+﻿namespace eAgenda.Core.Dominio.Compartilhado;
+
+public abstract class EntidadeBase<T>
+{
+    public Guid Id { get; set; }
+    public Guid UsuarioId { get; set; }
+
+    public abstract void AtualizarRegistro(T registroEditado);
+}
